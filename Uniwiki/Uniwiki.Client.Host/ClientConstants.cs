79178dfe405b4b8001330e98b413d48913d18999
Petr Šindelář <petr.sinde@gmail.com>
2020-07-14 11:51:07 +0300

    Fixed versioning

--- Uniwiki/Uniwiki.Client.Host/ClientConstants.cs
+++ Uniwiki/Uniwiki.Client.Host/ClientConstants.cs
@@ -4,6 +4,8 @@ namespace Uniwiki.Client.Host
 {
     public static class ClientConstants
     {
-        public static string AppVersionString => ((long)typeof(ClientConstants).Assembly.GetName().Version.Build + (long)typeof(Constants).Assembly.GetName().Version.Build).ToString();
+        public static string AppVersionString => //((long)typeof(ClientConstants).Assembly.GetHashCode() + typeof(Constants).Assembly.GetHashCode()).ToString();
+            (typeof(ClientConstants).Assembly.GetName().Version.Build + typeof(ClientConstants).Assembly.GetName().Version.Revision
+            + (long)typeof(Constants).Assembly.GetName().Version.Build + typeof(Constants).Assembly.GetName().Version.Revision).ToString();
     }
 }
