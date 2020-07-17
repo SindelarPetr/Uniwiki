8e1c4fb5ba1c346c5851692d8acad1ad8be81a76
Petr Šindelář <petr.sinde@gmail.com>
2020-07-17 16:31:53 +0300

    Added comments to the client's file program.cs, enabled verification of scopes

--- Uniwiki/Uniwiki.Client.Host/Program.cs
+++ Uniwiki/Uniwiki.Client.Host/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Uniwiki.Client.Host.Extensions;
 
 namespace Uniwiki.Client.Host
@@ -14,21 +15,28 @@ namespace Uniwiki.Client.Host
         public static bool IsTest { get; set; } = false;
         public static async Task Main(string[] args)
         {
+            // Create the app builder. Uncover magic here https://github.com/dotnet/aspnetcore/blob/bbc116254accb4f279010afc1ceb08a5141d012b/src/Components/WebAssembly/WebAssembly/src/Hosting/WebAssemblyHostBuilder.cs
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            
-            foreach (var service in builder.Services)
-            {
-                Console.WriteLine(service.ServiceType.ToString());
-            }
 
+            // Add root component
+            builder.RootComponents.Add<App>("app"); 
+
+            // Add Uniwiki services
             builder.Services.AddUniwikiClientHostServices();
 
-            Console.WriteLine("Environment: " + builder.HostEnvironment.Environment);
+            // On Development environment validate the DI scopes
+            if (builder.HostEnvironment.IsDevelopment())
+                builder.Services.BuildServiceProvider(true);
 
+            Console.WriteLine("Client environment: " + builder.HostEnvironment.Environment);
+            
+            // Create the builder
             var host = builder.Build();
+
+            // Initialize the client - thing, which should be run before start of the app
             await host.InitializeClient(builder.HostEnvironment.BaseAddress);
 
+            // Run the app
             await host.RunAsync();
         }
     }
